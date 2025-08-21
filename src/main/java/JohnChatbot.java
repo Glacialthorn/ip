f2aaa582f1759d58463cb2ec2101ed6762919c10
Benjamin Lim <phantomgubiak@gmail.com>
2025-08-21 15:51:00 +0800

    Added echoing- Level-1

--- src/main/java/JohnChatbot.java
+++ src/main/java/JohnChatbot.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class JohnChatbot {
     public static void main(String[] args) {
         String lineBreak = "\n____________________________________________________________\n";
-        System.out.println(lineBreak + "Hello! I'm John Chatbot!\nWhat can I do for you?" +
-                            lineBreak + "Bye. Hope to see you again soon!" + lineBreak);
+        System.out.println(lineBreak + "Hello! I'm John Chatbot!\nWhat can I do for you?" + lineBreak);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")) {
+                System.out.println(lineBreak + "Bye. Hope to see you again soon!" + lineBreak);
+                break;
+            } else {
+                System.out.println(lineBreak + userInput + lineBreak);
+            }
+        }
     }
 }
