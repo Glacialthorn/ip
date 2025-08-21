defb9c00a38c1e937c07e1c2de021c53728f9846
Benjamin Lim <phantomgubiak@gmail.com>
2025-08-21 15:33:14 +0800

    Level-0

---
+++ src/main/java/JohnChatbot.java
@@ -0,0 +1,7 @@
+public class JohnChatbot {
+    public static void main(String[] args) {
+        String lineBreak = "\n____________________________________________________________\n";
+        System.out.println(lineBreak + "Hello! I'm John Chatbot!\nWhat can I do for you?" +
+                            lineBreak + "Bye. Hope to see you again soon!" + lineBreak);
+    }
+}
