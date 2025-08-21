9eae0d8104d408cbd61793af2bd3326e7b0ad1d4
Benjamin Lim <phantomgubiak@gmail.com>
2025-08-21 16:52:27 +0800

    Added marking and unmarking Tasks as done - Level-3

--- src/main/java/JohnChatbot.java
+++ src/main/java/JohnChatbot.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class JohnChatbot {
     public static void main(String[] args) {
-        List<String> userInputs = new ArrayList<>();
+        List<Task> userInputs = new ArrayList<>();
 
         String lineBreak = "\n____________________________________________________________\n";
         System.out.println(lineBreak + "Hello! I'm John Chatbot!\nWhat can I do for you?" + lineBreak);
@@ -17,16 +17,55 @@ public class JohnChatbot {
                     break;
                 case "list":
                     System.out.println(lineBreak);
-                    for(int i = 0; i < userInputs.size(); i++){
-                        System.out.println((i + 1) + ". " + userInputs.get(i) + "\n");
+                    for(int i = 0; i < userInputs.size(); i++) {
+                        System.out.println((i + 1) + "." + getStatus(userInputs.get(i)) +
+                                            userInputs.get(i).getDescription() + "\n");
                     }
                     System.out.println(lineBreak);
                     break;
                 default:
+                    if (isMarkCommand(userInput)) {
+                        int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                        userInputs.get(taskIndex).markAsDone();
+                        System.out.println(lineBreak + "Nice! I've marked this task as done: \n" +
+                                            getStatus(userInputs.get(taskIndex)) +
+                                            userInputs.get(taskIndex).getDescription() + lineBreak);
+                        break;
+                    } else if (isUnmarkCommand(userInput)) {
+                        int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                        userInputs.get(taskIndex).markAsUndone();
+                        System.out.println(lineBreak + "OK, I've marked this task as not done yet: \n" +
+                                getStatus(userInputs.get(taskIndex)) +
+                                userInputs.get(taskIndex).getDescription() + lineBreak);
+                        break;
+                    }
                     System.out.println(lineBreak + "added: " + userInput + lineBreak);
-                    userInputs.add(userInput);
+                    userInputs.add(new Task(userInput));
                     break;
             }
         }
+    }
+
+    public static String getStatus(Task task) {
+        if (task.isDone) {
+            return "[X] ";
+        } else {
+            return "[ ] ";
+        }
+
+    }
+
+    public static boolean isMarkCommand(String input) {
+        String frontPart = input.substring(0, Math.min(input.length(), 4));
+        if (frontPart.contains("mark")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isUnmarkCommand(String input) {
+        String frontPart = input.substring(0, Math.min(input.length(), 6));
+        return frontPart.contains("unmark");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public boolean getStatus() {
+        return this.isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+    
+    
+}
